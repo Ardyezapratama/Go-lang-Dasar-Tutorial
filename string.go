f9fe9430d0887d11d86f6b08b45ed0164b0e385b
Eza Prat <64527082+Ardyezapratama@users.noreply.github.com>
2022-11-12 13:58:03 +0800

    Update string.go

--- string.go
+++ string.go
@@ -15,7 +15,7 @@ func main() {
 
 /*
 * Tipe Data String
-	- Stirng merupakan data kumpulan karakter
+	- String merupakan data kumpulan karakter
 	- Jumlah karkater di dalam String bisa nol sampai tidak terhingga
 	- Tipe data String di Go-lang drepresentasikan dengan kata string
 	- Nilai dat String di Go-lang selalu diawali dengan karakter "(petik dua) dan diakhiri dengan karakter "(petik dua)
