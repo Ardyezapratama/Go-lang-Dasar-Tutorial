626faf7194577fd17ecdcae4d8d53278bbc54bdc
Eza Prat <ardyezapratama@gmail.com>
2022-11-12 17:48:37 +0800

    Create and coding in boolean-operator.go

---
+++ boolean-operator.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main(){
+	var (
+		nilaiAkhir = 90
+		absensi = 80
+	)
+
+	fmt.Println(nilaiAkhir >= 80 && absensi >= 80)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* Operasi && (And)
+	- true && ture = true
+	- true && false = false
+	- false && true = false
+	-false && false = false
+
+* Operasi || (Or)
+	- true || true = true
+	- true  || false = true
+	- false || true = true
+	- false || false = false
+
+* Operasi !
+	- !true = false
+	- !false = true
+*/
