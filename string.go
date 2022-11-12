4ffe4f721047a04b6b26c8570830237c80c0e18c
Eza Prat <ardyezapratama@gmail.com>
2022-11-12 13:48:54 +0800

    Create and coding in string.go

---
+++ string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ardy")
+	fmt.Println("Ardy Eza")
+	fmt.Println("Ardy Eza Pratama")
+}
+
+/*
+* Tipe Data String
+	- Stirng merupakan data kumpulan karakter
+	- Jumlah karkater di dalam String bisa nol sampai tidak terhingga
+	- Tipe data String di Go-lang drepresentasikan dengan kata string
+	- Nilai dat String di Go-lang selalu diawali dengan karakter "(petik dua) dan diakhiri dengan karakter "(petik dua)
+*/
