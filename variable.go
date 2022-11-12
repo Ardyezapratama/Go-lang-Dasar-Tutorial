d35f4e3c742c905c90e920d3dcf760f466633e34
Eza Prat <ardyezapratama@gmail.com>
2022-11-12 14:18:45 +0800

    Create and coding in variable.go

---
+++ variable.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Eza Pratama"
+	fmt.Println(name)
+
+	name = "Ardy Eza"
+	fmt.Println(name)
+
+	var friendName = "Ezzzz"
+	fmt.Println(friendName)
+
+	var age = 22
+	fmt.Println(age)
+
+	country := "indonesia"
+	fmt.Println(country)
+
+	var (
+		firstname = "Ardy Eza"
+		lastname  = "Pratama"
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+
+}
