329c79e5a164e49668bf55d0e58d8cf32c53ea14
Eza Prat <ardyezapratama@gmail.com>
2022-11-12 13:56:27 +0800

    Update source code on string.go

--- string.go
+++ string.go
@@ -6,6 +6,11 @@ func main() {
 	fmt.Println("Ardy")
 	fmt.Println("Ardy Eza")
 	fmt.Println("Ardy Eza Pratama")
+
+	fmt.Println(len("Ardy"))
+	fmt.Println("Ardy Eza"[0])
+	fmt.Println("Ardy Eza Pratama"[2])
+
 }
 
 /*
@@ -14,4 +19,8 @@ func main() {
 	- Jumlah karkater di dalam String bisa nol sampai tidak terhingga
 	- Tipe data String di Go-lang drepresentasikan dengan kata string
 	- Nilai dat String di Go-lang selalu diawali dengan karakter "(petik dua) dan diakhiri dengan karakter "(petik dua)
+
+*Function untuk String
+	- len("string") untuk menghitung jumlah karakter di String
+	- "string[number]" untuk mengambil karakter pada posisi yang ditentukan
 */
