fd65f0449fec013fc71acec70ff70df210b5eb89
Eza Prat <ardyezapratama@gmail.com>
2022-11-12 13:34:23 +0800

    Create and coding file datatype.go

---
+++ datatype.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga Koma Lima = ", 3.5)
+}
+
+/*Tipe Data Number
+- ada dua jenis tipe data Number, yaitu:
+	- Integer
+	- Floating Point
+
+* Tipe Data Integer (1)
+int8 = -128 samapi 127
+int16 = -32768 sampai 32767
+int32 = -2147483648 sampai 2147483647
+int68 = -9223372036854775808 sampai 9223372036854775807
+
+* Tipe Data Integer (2)
+uint8 = 0 - 255
+uint16 = 0 - 65535
+uint32 = 0 - 4294967295
+uint64 = 0 - 18446744073709551615
+
+
+* Tipe Data Floating Point
+float32 = 1.18x10^-38 sampai 3.4x10^38
+float64 = 2.23x10^-308 sampai 1.80x10^308
+
+* Alias 
+merupakan nama lain untuk tipe data yang sudah ada
+byte = uint8
+rune = int32
+int = minimal int32
+uint = minimal uint 32
