429a50864e74f14595c7661af69e2a0ccc0b9a95
Eza Prat <ardyezapratama@gmail.com>
2022-11-13 22:57:57 +0800

    type assertion

---
+++ type-assertion.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func random() interface{} {
+	return true
+}
+
+func main() {
+	// result := random()
+	// resultString := result.(string)
+	// fmt.Println(resultString)
+
+	// resultInt := result.(int) // panix
+	// fmt.Println(resultInt)
+
+	result := random()
+	switch value := result.(type) {
+	case string:
+		fmt.Println("String", value)
+	case int:
+		fmt.Println("Int", value)
+	case bool:
+		fmt.Println("Boolean", value)
+	default:
+		fmt.Println("Unknown")
+	}
+}
