5fc60020a96f0760d36fdfa19fcc4015256e51b0
Eza Prat <ardyezapratama@gmail.com>
2022-12-14 18:59:48 +0800

    update Source Code

--- pointer.go
+++ pointer.go
@@ -5,6 +5,11 @@ type Address struct{
 	City, Province, Country string
 }
 
+func ChangeCountryToIndonesia(address *Address){
+	address.Country = "Indonesia"
+
+}
+
 func main(){
 	address1 := Address{"Banyuwangi","Jawa Timur", "Indonesia"}
 	// address2 := address1 // pointer pass by value
@@ -24,4 +29,13 @@ func main(){
 	address4 := new(Address)
 	address4.City = "Denpasar"
 	fmt.Println(address4)
+
+	alamat := &Address{
+		City: "Subang",
+		Province: "Jawa Barat",
+		Country: "",
+
+	}
+	ChangeCountryToIndonesia(alamat)
+	fmt.Println(alamat)
 }
