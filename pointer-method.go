80edc802d09f6f223d91f26c89eced7f2e67f10a
Eza Prat <ardyezapratama@gmail.com>
2022-12-14 20:14:42 +0800

    Update in pointer method

--- pointer-method.go
+++ pointer-method.go
@@ -15,3 +15,9 @@ func main(){
 	eza.Married()
 	fmt.Println(eza.Name)
 }
+
+/* 
+-	Walaupun method akan menempel di struct, tapi sebenarnya dat struct yang diakses didalam
+	method adalah pass by value
+-	Sangat direkomendasikan menggunakan pointer di method, sehingga tidak boros memory karena
+	harus selalu diduplikasi ketika membuat method
